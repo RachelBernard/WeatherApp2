49d443fd715fd7d72395fd75de3a194c2d9e8dd6
RC <rach.c.bern@gmail.com>
2021-09-23 14:22:43 -0400

    Update index.js
    fixing high function

--- src/index.js
+++ src/index.js
@@ -79,7 +79,8 @@ function changeToCel(event) {
   cel.classList.add("active");
   let celTemp = (fahrenheitTemp - 32) / 1.8;
   temperatureElement.innerHTML = Math.round(celTemp);
-  highTempElement.innerHTML = Math.round(celTemp);
+  let highTemp = (high - 32) / 1.8;
+  highTempElement.innerHTML = Math.round(highTemp);
 }
 
 function changeToFar(event) {
@@ -97,3 +98,5 @@ let far = document.querySelector("#far");
 far.addEventListener("click", changeToFar);
 
 let fahrenheitTemp = null;
+let high = null;
+let low = null;
